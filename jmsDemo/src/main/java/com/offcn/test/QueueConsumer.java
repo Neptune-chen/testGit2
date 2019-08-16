0998a9a4f6bf28077f5dcaba6f202d89a8f70c65
chen <1149182892@qq.com>
2019-08-16 20:23:58 +0800

    冲突测试

--- jmsDemo/src/main/java/com/offcn/test/QueueConsumer.java
+++ jmsDemo/src/main/java/com/offcn/test/QueueConsumer.java
@@ -18,9 +18,9 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class QueueConsumer {
 
 	public static void main(String[] args) {
-		System.out.println("测试git");
-		System.out.println("测试分支bate");
-		12
+//		System.out.println("测试git");
+//		System.out.println("测试分支bate");
+//		24
 		try {
 			//创建连接工厂
 			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.188.128:61616");
